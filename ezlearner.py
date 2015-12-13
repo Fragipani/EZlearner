9f4eb843cbd272049a02672329ba2bd3c902c8f0
raykw <rayk.wichmann@gmx.de>
2015-12-13 15:08:53 +0100

    test eins zwei nase

---
+++ ezlearner.py
@@ -0,0 +1,21 @@
+__author__ = 'Rayk, Thomas'
+
+import urllib.request, nltk
+
+
+# Import zufaellig ausgesuchter DE Text
+url = "http://www.gutenberg.org/cache/epub/24377/pg24377.txt"
+response = urllib.request.urlopen(url)
+raw_text = response.read().decode('utf8')
+print(raw_text)
+
+#Buchtext definieren.
+start_bookmark = raw_text.find("Erstes Kapitel")
+end_bookmark = raw_text.rfind("Im Verlag von R")
+text = raw_text[start_bookmark:end_bookmark]
+
+#Saetze tokenizieren
+sentence_tokens = nltk.sent_tokenize(text)
+
+# warum ist die Liste sentence_tokens leer? In der Console nicht...
+# Umgang mit Umlauten
