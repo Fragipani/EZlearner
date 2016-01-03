2508a6d5e745c779bdff7600d04b20bbbe831234
Thomas Pfister <th.pfister@gmail.com>
2016-01-03 11:21:32 +0100

    Prints sentence with gap and some possible answers. Checks for correct input

---
+++ ui/userInput.py
@@ -0,0 +1,59 @@
+
+import nltk, string
+
+from random import randint
+
+#Import from txt file
+
+f = open("../ressources/reiseberichtIndien.txt")
+raw_text = f.read()
+#print(raw_text)
+
+#Buchtext definieren.
+start_bookmark = raw_text.find("Erstes Kapitel")
+end_bookmark = raw_text.rfind("Im Verlag von R")
+text = raw_text[start_bookmark:end_bookmark]
+
+
+tokens = nltk.word_tokenize(text, language='german')
+sentence_tokens = nltk.sent_tokenize(text, language='german')
+
+randSentence = sentence_tokens[randint(0,len(sentence_tokens))]
+
+randSentenceTokens = nltk.word_tokenize(randSentence, language='german')
+
+
+
+
+randNumberTokens = randint(0,len(randSentenceTokens))
+cutWord = randSentenceTokens[randNumberTokens]
+randSentenceTokens[randNumberTokens]='________'
+
+
+
+newSent ="".join([" "+i if not i.startswith("'") and i not in string.punctuation else i for i in randSentenceTokens]).strip()
+print("Satz: " + newSent)
+
+#print("Lueckenwort: " + cutWord)
+
+text = nltk.Text(word.lower() for word in tokens)
+
+
+
+idx = nltk.text.ContextIndex([word.lower( ) for word in tokens])
+
+save = idx.similar_words(cutWord, n=3)
+save.append(cutWord)
+
+print("Moegliche Woerter:")
+print(save)
+
+response = input("Fehlendes Wort eingeben: ")
+
+if response == cutWord:
+ print("Korrekt")
+else:
+ print("Falsch!!!!")
+
+
+
