8f7488b176862618f501d668c58bdf0d48da7171
raykw <rayk.wichmann@gmx.de>
2016-02-29 14:52:51 +0100

    added function getPOSrank

--- Difficulty/SentenceDiff.py
+++ Difficulty/SentenceDiff.py
@@ -16,10 +16,72 @@ def getSentenceLength(sentenceTokens):
     return len(sentenceTokens)
 
 def getPosRank(sentence):
-# 
+    # Definition of POS Container
+    adjektive = ['ADJA', 'ADJD', 'PAV']
+    praepositionen = ['ADV', 'APPR', 'APPRART', 'APPO', 'APZR']
+    # weiteres = ['Art', 'CARD', 'FM', 'ITJ', 'ORD', 'PTKNEG', 'PTKANT', 'PTKA', 'SGML', 'SPELL', 'TRUNC', 'XY', '$,', '$.', '$(']
+    nomen_Eigenwörter = ['NN', 'NE']
+    pronomen = ['PDS', 'PDAT', 'PIS', 'PIAT', 'PIDAT', 'PPER', 'PPOSS', 'PPOSAT', 'PRELS', 'PRELAT', 'PRF', 'PWS', 'PWAT', 'PWAV']
+    verben_konjugationen = ['KOUI', 'KOUS', 'KON', 'KOKOM', 'PTKZU', 'PTKVZ', 'VVFIN', 'VVIMP', 'VVINF', 'VVIZU', 'VVPP', 'VAFIN', 'VAIMP', 'VAINF', 'VAPP', 'VMFIN', 'VVINF', 'VMPP']
+    # Reset
+    adj=0
+    pra=0
+    wei=0
+    nom=0
+    pro=0
+    ver=0
+    # Counting occurences of Containers in sentence
     pos_sentence = st.tag(sentence)
+    for count_pos_tags in pos_sentence:
+        if count_pos_tags[1] in adjektive:
+            adj=adj+1
+        if count_pos_tags[1] in praepositionen:
+            pra=pra+1
+        if count_pos_tags[1] in nomen_Eigenwörter:
+            nom=nom+1
+        if count_pos_tags[1] in pronomen:
+            pro=pro+1
+        if count_pos_tags[1] in verben_konjugationen:
+            ver=ver+1
+        else:
+            wei=wei+1
 
-    return pos_sentence
+    #Weighted occurence of containers
+    adjpercent = adj / len(pos_sentence)
+    prapercent = pra / len(pos_sentence)
+    nompercent = nom / len(pos_sentence)
+    propercent = pro / len(pos_sentence)
+    verpercent = ver / len(pos_sentence)
+    weipercent = 1 - adjpercent - prapercent - nompercent - propercent - verpercent
+    print(adjpercent, prapercent, nompercent, propercent, verpercent, weipercent)
+
+    #Compare sentence with preset values to indicate if this is a not usual sentence
+    deviation=0
+    if 0.05 < adjpercent < 0.15:
+        deviation=deviation+1
+    if 0.1 < prapercent < 0.2:
+        deviation=deviation+1
+    if 0.25 < nompercent < 0.35:
+        deviation=deviation+1
+    if 0.1 < propercent < 0.2:
+        deviation=deviation+1
+    if 0.5 < verpercent < 1.5:
+        deviation=deviation+1
+    if 0.15 < weipercent < 0.25:
+        deviation=deviation+1
+
+    if deviation == 6:
+        calc_value = 5
+    elif deviation == 5:
+        calc_value = 4
+    elif deviation == 4:
+        calc_value = 3
+    elif deviation == 3:
+        calc_value = 2
+    elif deviation <= 2:
+        calc_value = 1
+
+    return calc_value
 
 def getWordDifficulty (sentenceTokens):
     WordDiffList = list()
@@ -40,7 +102,8 @@ def getWordDifficulty (sentenceTokens):
 
 randSentenceTokens = ['Ihr', 'Haupt', 'verdunkelte', 'die', 'Ampel', ',', 'so', 'daß', 'ihre', 'Gestalt', 'in', 'magischen', 'Lichträndern', 'glomm', '.']
 #randSentenceTokens = ['Er', 'hat', 'das', 'mit', '"', 'I', 'love', 'you', '"', 'übersetzt']
-print(randSentenceTokens)
-print("Sentence length: " + str(getSentenceLength(randSentenceTokens)))
+#print(randSentenceTokens)
+#print("Sentence length: " + str(getSentenceLength(randSentenceTokens)))
 
-print(getWordDifficulty(randSentenceTokens))
+#print(getWordDifficulty(randSentenceTokens))
+print(getPosRank(randSentenceTokens))
